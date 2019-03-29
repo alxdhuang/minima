5737a094668bbd0d741033bfc09e2307ea414628
alxdhuang <alxdhuang@gmail.com>
2019-03-30 00:32:12 +0800

    v0.7.2: according to the hash value of a tag to decide its color

--- assets/js/color.js
+++ assets/js/color.js
@@ -1,3 +1,20 @@
+/**
+ * Refer to https://cp-algorithms.com/string/string-hashing.html
+ * @param {string} str 
+ */
+function hash(str) {
+    const p = 31;
+    const m = 1e9 + 9;
+    let h = 0;
+    let pow = 1;
+    for (let i = 0; i < str.length; i++) {
+        const c = str.charCodeAt(i);
+        h = (h + (c - 'a'.charCodeAt(0) + 1) * pow) % m;
+        pow = (pow * p) % m;
+    }
+    return h;
+}
+
 function invertColor(hex, bw) {
     if (hex.indexOf('#') === 0) {
         hex = hex.slice(1);
@@ -25,6 +42,7 @@ function invertColor(hex, bw) {
     // pad each with zeros and return
     return "#" + padZero(r) + padZero(g) + padZero(b);
 }
+
 function padZero(str, len) {
     len = len || 2;
     var zeros = new Array(len).join('0');
@@ -36,11 +54,16 @@ function randomColor() {
     return '#' + padZero(color, 6);
 }
 
+function hashColor(str) {
+    color = (hash(str) % 16777215).toString(16);
+    return '#' + padZero(color, 6);
+}
+
 function setInvertedColor(className) {
     let elems = document.getElementsByClassName(className);
     for (const elem of elems) {
-        const bgColor = randomColor();
-        let color = invertColor(bgColor, true);
+        const bgColor = hashColor(elem.innerText);
+        const color = invertColor(bgColor, true);
         elem.style['color'] = color;
         elem.style['background-color'] = bgColor;
     }
